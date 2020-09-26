e35efb16c8af54d7cae0b4ae40a4b3c2d0784a07
Harzinn <nickkrugz@gmail.com>
2020-09-26 19:40:29 -0400

    Fixed formatting, added bet check

--- Games of Chance/games.py
+++ Games of Chance/games.py
@@ -6,41 +6,75 @@ money = 100
 # Write your game of chance functions here
 
 def start_coin():
+    # Starting game logic
     print("Heads or Tails!\n")
-    bet = int(input("How much money do you want to bet?  You currently have " + str(money) + " dollars."))
-    choice = int(input("Heads (1) or Tails (2) ?"))
-    coin_game(choice, bet)
-    another = input("Do you want to play again? (yes or no) ")
-    coin_play_again(another)
+    # Take the bet from the user
+    bet = int(input("How much money do you want to bet?  You currently have " + str(money) + " dollars.\n"))
+    # Make sure the bet is valid
+    checked = bet_check(bet, money)
+    # If the bet isn't valid, restart the start_coin() function
+    if checked:
+        print("You don't have that much try again\n")
+        start_coin()
+    # If the be is valid, continue on
+    elif not checked:
+        # Take input on heads or tails for the coin and save it to choice
+        choice = int(input("Heads (1) or Tails (2) ?"))
+        # check to make sure that the user input is valid with coin_game_valid function
+        coin_game_valid(choice, bet)
+        # make sure we have more than 0 dollars and check if we want to play again
+        if money > 0:
+            another = input("Do you want to play again? (yes or no) ")
+            # take choice and pass to the play again function
+            coin_play_again(another)
+        else:
+            print("Go home your broke")
+    else:
+        print("Confused")
 
 
-def coin_flip(input2, bet):
+# lets make sure that we arent betting more than we have
+def bet_check(bet, money):
+    if bet > money:
+        return True
+    else:
+        return False
+
+
+def coin_flip(choice, bet):
+    # using global money to save the values later
     global money
+    # randomize a choice between 1 and 2 and save as result
     result = random.randint(1, 2)
-    if input2 == result:
+    # if we win
+    if choice == result:
         money += bet
         if result == 1:
             return "You win " + str(bet) + " dollars! It's Heads! You have have " + str(money) + " dollars total!"
         else:
             return "You win " + str(bet) + " dollars! It's Tails! You have have " + str(money) + " dollars total!"
-    elif input2 != result:
+    # if we don't win
+    elif choice != result:
         money -= bet
         if result == 1:
             return "You lose " + str(bet) + " dollars! It's Heads! You have have " + str(money) + " dollars total!"
         else:
             return "You lose " + str(bet) + " dollars! It's Tails! You have have " + str(money) + " dollars total!"
+    # if things get strange
     else:
         return "Not a valid input, try again"
 
 
-def coin_game(check, bet):
+def coin_game_valid(check, bet):
     # error check if not 1 or 2
     if check >= 3:
         print("Not a valid option")
-        choice1 = int(input("Heads (1) or Tails (2) ?"))
-        coin_game(choice1, bet)
+        choice = int(input("Heads (1) or Tails (2) ?"))
+        # re-run the check with new choice
+        coin_game_valid(choice, bet)
     # otherwise run the coin flip
     else:
+        # run the coin flip function normally
         print(coin_flip(check, bet))
 
 
